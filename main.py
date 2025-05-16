64457efb5b8cd4aaaa4bb73065e232954407d304
Jessé Menegat <eng.soft.jesse@gmail.com>
2025-05-15 23:41:44 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+
+
+# Criando a janela
+window = pygame.display.set_mode(size=(600,480)) #Initialize a window or screen for display
+
+print('Setup End')
+while True:
+    # check for all events https://www.pygame.org/docs/ref/event.html
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close Window
+            quit() # Encerrar --  end pygame
+
+#
